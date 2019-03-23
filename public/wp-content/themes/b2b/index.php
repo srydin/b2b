e3a13209d0642add76d632719c59ba3088fc7791
srydin <stevenrydin@gmail.com>
2019-03-23 09:53:06 -0600

    removed old themes, introduced empty b2b theme

---
+++ public/wp-content/themes/b2b/index.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ */
+
+//get_header();
+?>
+
+	<section id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+		<?php
+		if ( have_posts() ) {
+
+			// Load posts loop.
+			while ( have_posts() ) {
+				the_post();
+			}
+
+
+		} else {
+
+        echo 'Sorry, no posts found.';
+		}
+		?>
+
+		</main><!-- .site-main -->
+	</section><!-- .content-area -->
+
+<?php
+//get_footer();
